5ba44f05701d907bc8ff86a99f18cfb837110101
malec <alec@ahlbrandt.net>
2019-04-08 09:12:00 -0500

    working

--- pa7.sbt
+++ pa7.sbt
@@ -1,4 +1,4 @@
-name := "Word COunt"
+name := "Word Count"
 
 version := "1.0"
 
--- wordcount.scala
+++ wordcount.scala
@@ -8,10 +8,7 @@ object SimpleApp {
     val conf = new SparkConf().setAppName("Word Count")
     val sc = new SparkContext(conf)
     var lines = sc.textFile("input.txt")
-    var count = lines.map(line => line.split(" ").filter(word => word != "").length).reduce((acc, lineCount) => acc + lineCount)
-    val writer = new PrintWriter(new File("result.txt" ))
-    writer.write(count.toString())
-    writer.close()
-    sc.stop()
+    val wordMap = scala.collection.mutable.Map[String, Int]().withDefaultValue(0)
+    sc.parallelize(lines.flatMap(_.split("\\W+")).collect.toList.groupBy(word => word).mapValues(value => value.length).toList, 1).saveAsTextFile("output")
   }
 }
