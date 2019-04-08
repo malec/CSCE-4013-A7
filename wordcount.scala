c2d4551e17fb1668e24cd76339c98c53f39a61b9
malec <alec@ahlbrandt.net>
2019-04-08 11:11:53 -0600

    remove unused map

--- wordcount.scala
+++ wordcount.scala
@@ -1,14 +1,12 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import java.io._
 
-object SimpleApp {
+object WordCount {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Word Count")
     val sc = new SparkContext(conf)
     var lines = sc.textFile("input.txt")
-    val wordMap = scala.collection.mutable.Map[String, Int]().withDefaultValue(0)
     sc.parallelize(lines.flatMap(_.split("\\W+")).collect.toList.groupBy(word => word).mapValues(value => value.length).toList, 1).saveAsTextFile("output")
   }
 }
